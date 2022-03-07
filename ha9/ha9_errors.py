8198d14537f2ebcb754f032185e6f5900503668c
Alex Kaufman <alexanderkaufman@montana.edu>
2022-03-07 09:26:21 -0700

    adding more error types based on manual

--- ha9/ha9_errors.py
+++ ha9/ha9_errors.py
@@ -1,2 +1,17 @@
-class HA9_Exception(Exception):
+class HA9Exception(Exception):
+    pass
+
+class SelfTestError(HA9Exception):
+    pass
+
+class ServiceRequest(HA9Exception):
+    pass
+
+class SyntaxError(HA9Exception):
+    pass
+
+class MessageAvailableException(HA9Exception):
+    pass
+
+class ParameterError(HA9Exception):
     pass
